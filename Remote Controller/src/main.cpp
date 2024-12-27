cdfed36bcb1590b82334192a483c3563b01365c5
Ethan Hu <ethanhu8351@gmail.com>
2024-12-26 18:07:51 -0800

    working esp now

--- Remote Controller/src/main.cpp
+++ Remote Controller/src/main.cpp
@@ -1,6 +1,7 @@
-#include <WifiClient.h>
-#include <stdint.h>
+#include <esp_now.h>
 #include <WiFi.h>
+#include <esp_wifi.h>
+#include <stdint.h>
 enum ControlMode
 {
 	DUTY = 0,
@@ -33,184 +34,153 @@ struct VescDataPackage
 	bool timeoutSwitchActive;
 };
 
-// function header
-void sendTCPMessage(RemoteDataPackage message);
-bool receiveTCPMessage();
-void buffer_append_int32(uint8_t *buffer, int32_t number, int32_t *index);
-void buffer_append_float32(uint8_t *buffer, float number, float scale, int32_t *index);
-int32_t buffer_get_int32(const uint8_t *buffer, int32_t *index);
-float buffer_get_float32(const uint8_t *buffer, float scale, int32_t *index);
+// function prototypes
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len);
+void readMacAddress();
+void printVescValues();
 
-// WiFi credentials
-const char *ssid = "ESP32_AP";
-const char *password = "password";
+// REPLACE WITH THE MAC Address of your receiver
+// uint8_t broadcastAddress[] = {0x54, 0x43, 0xb2, 0xac, 0xee, 0xf8};
+uint8_t broadcastAddress[] = {0xc8, 0xc9, 0xa3, 0xce, 0xff, 0xd0};
+// Variable to store if sending data was successful
+String success;
+esp_now_peer_info_t peerInfo;
 RemoteDataPackage remoteData;
 VescDataPackage vescData;
 
-// TCP setup
-WiFiClient tcpClient;
-IPAddress serverIP(192, 168, 1, 1); // IP of the server
-unsigned int serverPort = 4210;		// Port of the server
-
 void setup()
 {
+	// Init Serial Monitor
 	Serial.begin(9600);
-	Serial.println();
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-	Serial.println("\nConnecting to WiFi Network ..");
 
-	while (WiFi.status() != WL_CONNECTED)
+	// Set device as a Wi-Fi Station
+	WiFi.mode(WIFI_STA);
+	Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
+	readMacAddress();
+
+	// Init ESP-NOW
+	if (esp_now_init() != ESP_OK)
 	{
-		Serial.print(".");
-		delay(100);
+		Serial.println("Error initializing ESP-NOW");
+		return;
 	}
 
-	Serial.println("\nConnected to the WiFi network");
-	Serial.print("Local ESP32 IP: ");
-	Serial.println(WiFi.localIP());
+	// Once ESPNow is successfully Init, we will register for Send CB to
+	// get the status of Trasnmitted packet
+	esp_now_register_send_cb(OnDataSent);
 
-	// Begin TCP
-	tcpClient.connect(serverIP, serverPort);
+	// Register peer
+	memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+	peerInfo.channel = 0;
+	peerInfo.encrypt = false;
+
+	// Add peer
+	if (esp_now_add_peer(&peerInfo) != ESP_OK)
+	{
+		Serial.println("Failed to add peer");
+		return;
+	}
+	// Register for a callback function that will be called when data is received
+	esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+	remoteData.controlMode = ControlMode::DUTY;
+}
+
+void getReadings()
+{
+	remoteData.dutyCycle = 0.2;
 	remoteData.controlMode = ControlMode::DUTY;
 }
 
 void loop()
 {
-	// continue only if the client is connected
-	if (tcpClient.connected())
+	getReadings();
+	esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&remoteData, sizeof(remoteData));
+
+	if (result == ESP_OK)
 	{
-		Serial.println("Reconnecting to server");
-		tcpClient.connect(serverIP, serverPort);
+		Serial.println("Sent with success");
 	}
 	else
 	{
-		Serial.println("Server connected");
-		remoteData.dutyCycle = 0.2;
-		sendTCPMessage(remoteData);
-		receiveTCPMessage();
+		Serial.println("Error sending the data");
 	}
+
 }
 
-void sendTCPMessage(RemoteDataPackage message)
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
-	// Create a packet to send
-	uint8_t packet[255];
-	// add the data to the packet
-	int index = 0;
-	buffer_append_float32(packet, message.dutyCycle, 1000.0, &index);
-	buffer_append_float32(packet, message.current, 100.0, &index);
-	packet[index++] = message.controlMode;
-
-	// Send a response back to the server
-	Serial.print("Sending packet of size: ");
-	Serial.println(index);
-
-	tcpClient.write(packet, index);
-}
-
-bool receiveTCPMessage()
-{
-	uint8_t incomingPacket[255]; // Buffer for incoming packets
-	// recieve the packet and get the size
-	int packetSize = tcpClient.available();
-
-	// read the remote packet into the remoteDataPackage struct
-
-	if (packetSize)
+	Serial.print("\r\nLast Packet Send Status:\t");
+	Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+	if (status == 0)
 	{
-		Serial.printf("Received %d bytes from %s, port %d\n", packetSize, tcpClient.remoteIP().toString().c_str(), tcpClient.remotePort());
-		tcpClient.read(incomingPacket, packetSize);
-		int index = 0;
-		vescData.avgMotorCurrent = buffer_get_float32(incomingPacket, 100.0, &index);
-		vescData.avgInputCurrent = buffer_get_float32(incomingPacket, 100.0, &index);
-		vescData.dutyCycleNow = buffer_get_float32(incomingPacket, 1000.0, &index);
-		vescData.rpm = buffer_get_float32(incomingPacket, 1.0, &index);
-		vescData.inpVoltage = buffer_get_float32(incomingPacket, 10.0, &index);
-		vescData.ampHours = buffer_get_float32(incomingPacket, 10000.0, &index);
-		vescData.ampHoursCharged = buffer_get_float32(incomingPacket, 10000.0, &index);
-		vescData.wattHours = buffer_get_float32(incomingPacket, 10000.0, &index);
-		vescData.wattHoursCharged = buffer_get_float32(incomingPacket, 10000.0, &index);
-		vescData.tachometer = buffer_get_int32((uint8_t *)incomingPacket, &index);
-		vescData.tachometerAbs = buffer_get_int32((uint8_t *)incomingPacket, &index);
-		vescData.pidPos = buffer_get_float32(incomingPacket, 1000000.0, &index);
-		vescData.id = incomingPacket[index++];
-		vescData.faultCode = incomingPacket[index++];
-		vescData.timedOut = incomingPacket[index++] == 1;
-		vescData.timeoutSwitchActive = incomingPacket[index++] == 1;
-
-		return true;
+		success = "Delivery Success :)";
 	}
 	else
 	{
-		Serial.println("No data received");
-
-		return false;
+		success = "Delivery Fail :(";
 	}
 }
 
-void buffer_append_int32(uint8_t *buffer, int32_t number, int32_t *index)
+// Callback when data is received
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
-	buffer[(*index)++] = (number >> 24) & 0xFF;
-	buffer[(*index)++] = (number >> 16) & 0xFF;
-	buffer[(*index)++] = (number >> 8) & 0xFF;
-	buffer[(*index)++] = number & 0xFF;
-}
+	Serial.println("Data received");
+	// parse the received data, typecast it to uint8_t
+	memcpy(&vescData, incomingData, sizeof(vescData));
 
-void buffer_append_float32(uint8_t *buffer, float number, float scale, int32_t *index)
-{
-	int32_t num = (int32_t)(number * scale);
-	buffer_append_int32(buffer, num, index);
-}
-
-int32_t buffer_get_int32(const uint8_t *buffer, int32_t *index)
-{
-	uint32_t res = ((uint32_t)buffer[*index]) << 24 |
-				   ((uint32_t)buffer[*index + 1]) << 16 |
-				   ((uint32_t)buffer[*index + 2]) << 8 |
-				   buffer[*index + 3];
-	*index += 4;
-	return res;
-}
-
-float buffer_get_float32(const uint8_t *buffer, float scale, int32_t *index)
-{
-	int32_t num = buffer_get_int32(buffer, index);
-	return (float)num / scale;
+	// print the received data
+	printVescValues();
 }
 
 void printVescValues()
 {
-	Serial.print("Motor Current: ");
-	Serial.println(vescData.avgMotorCurrent);
-	Serial.print("Input Current: ");
-	Serial.println(vescData.avgInputCurrent);
-	Serial.print("Duty Cycle: ");
-	Serial.println(vescData.dutyCycleNow);
-	Serial.print("RPM: ");
-	Serial.println(vescData.rpm);
-	Serial.print("Input Voltage: ");
-	Serial.println(vescData.inpVoltage);
-	Serial.print("Amp Hours: ");
-	Serial.println(vescData.ampHours);
-	Serial.print("Amp Hours Charged: ");
-	Serial.println(vescData.ampHoursCharged);
-	Serial.print("Watt Hours: ");
-	Serial.println(vescData.wattHours);
-	Serial.print("Watt Hours Charged: ");
-	Serial.println(vescData.wattHoursCharged);
-	Serial.print("Tachometer: ");
-	Serial.println(vescData.tachometer);
-	Serial.print("Tachometer Abs: ");
-	Serial.println(vescData.tachometerAbs);
-	Serial.print("PID Position: ");
-	Serial.println(vescData.pidPos);
+	// Serial.print("Motor Current: ");
+	// Serial.println(vescData.avgMotorCurrent);
+	// Serial.print("Input Current: ");
+	// Serial.println(vescData.avgInputCurrent);
+	// Serial.print("Duty Cycle: ");
+	// Serial.println(vescData.dutyCycleNow);
+	// Serial.print("RPM: ");
+	// Serial.println(vescData.rpm);
+	// Serial.print("Input Voltage: ");
+	// Serial.println(vescData.inpVoltage);
+	// Serial.print("Amp Hours: ");
+	// Serial.println(vescData.ampHours);
+	// Serial.print("Amp Hours Charged: ");
+	// Serial.println(vescData.ampHoursCharged);
+	// Serial.print("Watt Hours: ");
+	// Serial.println(vescData.wattHours);
+	// Serial.print("Watt Hours Charged: ");
+	// Serial.println(vescData.wattHoursCharged);
+	// Serial.print("Tachometer: ");
+	// Serial.println(vescData.tachometer);
+	// Serial.print("Tachometer Abs: ");
+	// Serial.println(vescData.tachometerAbs);
+	// Serial.print("PID Position: ");
+	// Serial.println(vescData.pidPos);
 	Serial.print("ID: ");
 	Serial.println(vescData.id);
-	Serial.print("Fault Code: ");
-	Serial.println(vescData.faultCode);
-	Serial.print("Timed Out: ");
-	Serial.println(vescData.timedOut);
-	Serial.print("Timeout Switch Active: ");
-	Serial.println(vescData.timeoutSwitchActive);
+	// Serial.print("Fault Code: ");
+	// Serial.println(vescData.faultCode);
+	// Serial.print("Timed Out: ");
+	// Serial.println(vescData.timedOut);
+	// Serial.print("Timeout Switch Active: ");
+	// Serial.println(vescData.timeoutSwitchActive);
+}
+void readMacAddress()
+{
+	uint8_t baseMac[6];
+	esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+	if (ret == ESP_OK)
+	{
+		Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+					  baseMac[0], baseMac[1], baseMac[2],
+					  baseMac[3], baseMac[4], baseMac[5]);
+	}
+	else
+	{
+		Serial.println("Failed to read MAC address");
+	}
 }
